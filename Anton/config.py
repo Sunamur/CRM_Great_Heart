1d23e78dfe4cd895c24a9c3942a2cd920fd896a9
Penitto <penitto02@gmail.com>
2020-11-10 15:54:32 +0300

    This is my version of crm

---
+++ Anton/config.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python2.7
+# Create dummy secrey key so we can use sessions
+SECRET_KEY = '1234__some__passwd'
+
+# Create in-memory database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///Directory.db'
+SQLALCHEMY_ECHO = True
+
+# Flask-Security config
+SECURITY_URL_PREFIX = "/"
+SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
+SECURITY_PASSWORD_SALT = "ATGUOHAELKiubahiughaerGOJAEGj"
+
+# Flask-Security URLs, overridden because they don't put a / at the end
+SECURITY_LOGIN_URL = "/login/"
+SECURITY_LOGOUT_URL = "/logout/"
+SECURITY_REGISTER_URL = "/"
+
+SECURITY_POST_LOGIN_VIEW = "/admin/"
+SECURITY_POST_LOGOUT_VIEW = "/"
+SECURITY_POST_REGISTER_VIEW = "/admin/"
+
+# Flask-Security features
+SECURITY_REGISTERABLE = False
+SECURITY_SEND_REGISTER_EMAIL = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
